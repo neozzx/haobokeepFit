a7d19adc266e88b52cc741f7c76c7e27bd1aa8ae
[KatieBoyum] <[neonp65@163.com]>
2018-06-05 21:47:45 +0800

    upload

---
+++ haobokeepFit/haobokeepFit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  haobokeepFit
+//
+//  Created by 震霄 张 on 2018/6/5.
+//  Copyright © 2018年 neo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
